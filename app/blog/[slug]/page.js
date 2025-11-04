a435d31f10f96020f2fc5f0ae691ac0fdb4eafd6
TJsohn <sohntaejeong@gmail.com>
2025-11-04 09:25:29 +0200

    Fix ESLint warnings (Image tags, dependency array, unescaped apostrophe)

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function SingleBlogPage() {
   const { slug } = useParams(); // Gets slug from the URL
@@ -44,9 +45,11 @@ export default function SingleBlogPage() {
       </h1>
 
       {imageUrl && (
-        <img
+        <Image
           src={imageUrl}
           alt={Title}
+          width={800}
+          height={400}
           style={{
             width: "100%",
             maxHeight: "400px",
