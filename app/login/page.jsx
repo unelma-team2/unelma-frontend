ffc4eb09a96bdf2281a7b159c87bd339898433cd
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-11-13 16:02:15 +0200

    update user authentication with Linkedin and Facebook

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -50,8 +50,8 @@ const Login = () => {
                     },
                     },
                 }}
-                providers={["google","github"]}
-                socialLayout="horizontal"
+                providers={["google", "github", "linkedin_oidc","facebook"]}
+                socialLayout="vertical"
                 view="sign_in"
                 />
             </Paper>
